2f344b6b7b5ab89fbb0f2dede367bca3e05d7aa8
fdmesquita <fannydemesquita@gmail.com>
2021-09-16 11:43:29 +0200

    normalize css and add main css in app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg'
 import './App.css'
+import './Normalize.css'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+        vestibulum risus eu massa cursus molestie. Ut dolor elit, vehicula at
+        fermentum ut, volutpat et ligula. Sed libero velit, scelerisque tempus
+        facilisis facilisis, dictum vel dolor. Vestibulum a aliquam dolor. Nam
+        euismod convallis nisl, ac aliquet dui pretium quis. Donec porttitor
+        faucibus sapien nec euismod. Sed consequat consequat urna, sed
+        vestibulum lectus. Nullam molestie turpis vel erat semper, quis
+        efficitur justo interdum. Praesent ut venenatis enim. In mattis magna
+        sit amet tortor commodo, sed vehicula purus viverra. Maecenas urna arcu,
+        viverra quis molestie nec, fringilla quis odio. Pellentesque quis
+        viverra lectus.
+      </p>
     </div>
   )
 }
