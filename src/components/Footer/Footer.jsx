80b1fa07e982b1788e4407258801f11dc2f9a41e
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-16 11:18:20 +0200

    add react-icon & react-router-dom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
+import { Link } from 'react-router'
+
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
+
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='wrapper'></div>
+      <div className='footerWrapper'>
+        <div className='footerSocial'>
+          <FaFacebook />
+          <FaInstagram />
+        </div>
+        <div className='footerLink'>
+          <Link to=''>Nous soutenir</Link>
+          <Link to=''>L’Association et ses partenaires</Link>
+          <Link to=''>Nous trouver</Link>
+          <Link to=''>On parle de nous</Link>
+        </div>
+      </div>
     </footer>
   )
 }
