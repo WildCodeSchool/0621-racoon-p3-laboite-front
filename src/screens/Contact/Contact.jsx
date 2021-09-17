6ca44ab5575617a88ad07d5898164c5dd92cf539
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-17 09:29:58 +0200

    contact v0.0.1

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { FaPhoneAlt } from 'react-icons/fa'
+import { FaPhoneAlt, FaFacebook, FaInstagram } from 'react-icons/fa'
 
 import './Contact.css'
 
@@ -12,7 +12,25 @@ const Contact = () => {
         <h2 className='contactRubbanTitle'>Contact</h2>
       </div>
 
-      <div className='contactInfo'></div>
+      <div className='contactInfoWrapper'>
+        <div className='contactInfo'>
+          <div className='contactInfoSocialLink'>
+            <FaFacebook />
+            <FaInstagram />
+          </div>
+          <div className='contactInfoText'></div>
+          <div className='contactInfoMap'>
+            <iframe
+              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43290.87063345045!2d0.9497721319364254!3d47.301064166248516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcb7cda60eab81%3A0x2291fbc7d1074edc!2zMzcxNTAgQmzDqXLDqQ!5e0!3m2!1sfr!2sfr!4v1631862573373!5m2!1sfr!2sfr'
+              width='100%'
+              height='120%'
+              allowFullScreen=''
+              loading='lazy'
+              title="Adress Boite d'acoté"
+            ></iframe>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
