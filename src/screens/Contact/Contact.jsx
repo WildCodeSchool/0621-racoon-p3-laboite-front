9d6059603bedf4984d996ef64e8a314258b4a87c
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-17 15:36:24 +0200

    modif width content

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -14,28 +14,29 @@ const Contact = () => {
 
       <div className='contactInfoWrapper'>
         <div className='contactInfo'>
-          <div className='contactInfoSocialLink'>
-            <FaFacebook />
-            <FaInstagram />
+          <div className='contactInfoContent'>
+            <div className='contactInfoSocialLink'>
+              <FaFacebook />
+              <FaInstagram />
+            </div>
+            <div className='contactInfoText'>
+              <h4>Notre adresse:</h4>
+              <p>1 rue du chemin, 37150 Bléré</p>
+              <p>
+                <FaPhoneAlt /> 06 54 50 50 50
+              </p>
+            </div>
+            <div className='contactInfoMap'>
+              <iframe
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43290.87063345045!2d0.9497721319364254!3d47.301064166248516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcb7cda60eab81%3A0x2291fbc7d1074edc!2zMzcxNTAgQmzDqXLDqQ!5e0!3m2!1sfr!2sfr!4v1631862573373!5m2!1sfr!2sfr'
+                width='130%'
+                height='180%'
+                allowFullScreen=''
+                loading='lazy'
+                title="Adress Boite d'acoté"
+              ></iframe>
+            </div>
           </div>
-          <div className='contactInfoText'>
-            <h4>Notre adresse:</h4>
-            <p>1 rue du chemin, 37150 Bléré</p>
-            <p>
-              <FaPhoneAlt /> 06 54 50 50 50
-            </p>
-          </div>
-          <div className='contactInfoMap'>
-            <iframe
-              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43290.87063345045!2d0.9497721319364254!3d47.301064166248516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcb7cda60eab81%3A0x2291fbc7d1074edc!2zMzcxNTAgQmzDqXLDqQ!5e0!3m2!1sfr!2sfr!4v1631862573373!5m2!1sfr!2sfr'
-              width='100%'
-              height='180%'
-              allowFullScreen=''
-              loading='lazy'
-              title="Adress Boite d'acoté"
-            ></iframe>
-          </div>
-          <div className='contactInfoSpace'></div>
         </div>
         <div className='contactMessage'>
           <h3>
