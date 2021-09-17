584b4e59590f24cc471f4cefa514073b6370fd30
Benjamin1er <benjaminj.geron@gmail.com>
2021-09-17 12:36:57 +0200

    Created oblic Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,18 +10,30 @@ const Header = () => {
         </div>
         <div className='header-text'>
           <h1>La boîte d'à côté...</h1>
-          <h1 style={{ paddingLeft: '60px' }}>
+          <h1 style={{ paddingLeft: '150px' }}>
             Une entreprise pas comme les autres !
           </h1>
         </div>
       </div>
-      <div className='navbar'>
-        <ul className='flex navlist'>
-          <li>Le Concept</li>
-          <li>Pôle Conciergerie</li>
-          <li>Pôle Recyclerie Créative</li>
-          <li>Pôle Végétal</li>
-        </ul>
+      <div className='flex navlist'>
+        <div>
+          <p style={{ color: '#EB5160' }}>N</p>
+        </div>
+        <div>
+          <p>Le Concept</p>
+        </div>
+        <div>
+          <p>Pôle Conciergerie</p>
+        </div>
+        <div>
+          <p>Pôle Recyclerie Créative</p>
+        </div>
+        <div>
+          <p>Pôle Végétal</p>
+        </div>
+        <div>
+          <p style={{ color: '#EB5160' }}>N</p>
+        </div>
       </div>
       <div className='blere-beach'></div>
     </div>
