27983ae001a6fdfa37b97aeebf83e7b1261deaf3
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-20 18:30:50 +0200

    test add google map

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+
 import { FaPhoneAlt, FaFacebook, FaInstagram } from 'react-icons/fa'
 
 import './Contact.css'
+import SimpleMap from './Map'
 
 const Contact = () => {
   // Infos de contact de la Boite d'acoté
-  const [contact, setContact] = useState({})
+  const [contact, setContact] = useState([])
 
   // Recupere les infos contact du back et bdd
   useEffect(() => {
@@ -33,31 +35,34 @@ const Contact = () => {
         <div className='contactInfo'>
           <div className='contactInfoContent'>
             <div className='contactInfoSocialLink'>
-              <a
-                href={contact.contact_social_link}
-                target='_blank'
-                rel='noreferrer'
-              >
-                <FaFacebook />
-              </a>
-              <FaInstagram />
+              {contact.map(link => (
+                <a
+                  href={link.contact_social_link}
+                  target='_blank'
+                  rel='noreferrer'
+                  key={link.id}
+                >
+                  <FaFacebook />
+                </a>
+              ))}
             </div>
             <div className='contactInfoText'>
               <h4>Notre adresse:</h4>
-              <p>{contact.contact_address}</p>
+              <p>{contact[0].contact_address}</p>
               <p>
-                <FaPhoneAlt /> {contact.contact_phone}
+                <FaPhoneAlt /> {contact[0].contact_phone}
               </p>
             </div>
             <div className='contactInfoMap'>
-              <iframe
+              {/* <iframe
                 src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43290.87063345045!2d0.9497721319364254!3d47.301064166248516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcb7cda60eab81%3A0x2291fbc7d1074edc!2zMzcxNTAgQmzDqXLDqQ!5e0!3m2!1sfr!2sfr!4v1631862573373!5m2!1sfr!2sfr'
                 width='130%'
                 height='180%'
                 allowFullScreen=''
                 loading='lazy'
                 title="Adress Boite d'acoté"
-              ></iframe>
+              ></iframe> */}
+              {/* <SimpleMap /> */}
             </div>
           </div>
         </div>
