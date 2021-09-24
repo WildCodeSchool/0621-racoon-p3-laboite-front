22490d86c11eeb049d8f1245a863388a9c85451b
Tristan <tristan.fluteaux@gmail.com>
2021-09-24 11:30:19 +0200

    [STYLE] add css hover polecard

--- src/components/Card/PoleCard.jsx
+++ src/components/Card/PoleCard.jsx
@@ -1,22 +1,36 @@
+import * as Icons from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core'
-import { faBoxOpen, faConciergeBell, faLeaf } from '@fortawesome/free-solid-svg-icons'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { NavLink } from 'react-router-dom'
 
 import './PoleCard.css'
-import powers from '../../assets/mini-2.jpeg'
 
-const PoleCard = ({id, pole_name, pole_picto}) => {
-  console.log('poulet devant', id)
+const PoleCard = ({ id, pole_title, pole_picto, pole_catchphrase, pole_miniature_img }) => {
+
+  const iconList = Object.keys(Icons)
+    .filter(key => key !== 'fas' && key !== 'prefix')
+    .map(icon => Icons[icon])
+
+  library.add(...iconList)
+
   return (
     <div className='polecard-container'>
-      <img className='polecard-image' src={powers} alt={pole_name} />
-      <div className='polecard-banner'>
-        <div className='polecard-picto'>
-          <FontAwesomeIcon icon={pole_picto}/>
+      <img
+        className='polecard-image'
+        src={pole_miniature_img}
+        alt={pole_title}
+      />
+      <NavLink to={`/pole/${id}`}>
+        <div className='polecard-banner'>
+          <div className='picto-container'>
+            <div className='polecard-picto'>
+              <FontAwesomeIcon icon='leaf' />
+            </div>
+          </div>
+          <h3>{pole_title}</h3>
+          <div className='catchPhrase'>{pole_catchphrase}</div>
         </div>
-        <h3>{pole_name}</h3>
-      </div>
+      </NavLink>
     </div>
   )
 }
