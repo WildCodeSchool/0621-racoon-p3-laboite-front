7cbc6f549d0da05f456480ecd8fe718bfac25a3a
LauraTrehout <lauratrehout@gmail.com>
2021-09-24 11:33:20 +0200

    fixed conflict in activitiespole and polefunc

--- src/components/ActivitiesPole/ActivitiesPole.jsx
+++ src/components/ActivitiesPole/ActivitiesPole.jsx
@@ -3,17 +3,6 @@ import './ActivitiesPole.css'
 
 const ActivitiesPole = ({ activity_desc, activity_img }) => {
   return (
-<<<<<<< HEAD
-    <div className='centerContainer'>
-      <section className='activitiesContainer'>
-        <div className='bothCreamContainer'>
-          <div className='leftCreamContainer'>
-            <img className='creamSection' src={activity_img} alt='services' />
-          </div>
-          <div className='rightCreamContainer'>
-            <p className='redLeftAlign'>{activity_desc}</p>
-          </div>
-=======
     <section className='activitiesContainer'>
       <div className='bothCreamContainer'>
         <div className='leftCreamContainer'>
@@ -21,7 +10,6 @@ const ActivitiesPole = ({ activity_desc, activity_img }) => {
         </div>
         <div className='rightCreamContainer'>
           <p className='redLeftAlign'>{activity_desc}</p>
->>>>>>> e7168e0c9a37a6a69ed0b5204a7e1a2c23afb8f8
         </div>
       </div>
     </section>
--- src/components/FuncPole/FuncPole.jsx
+++ src/components/FuncPole/FuncPole.jsx
@@ -8,24 +8,10 @@ const FuncPole = ({ pole_func, pole_func_img, pole_title }) => {
         <div className='titleLigns'>
           <h2 className='green'>Le fonctionnement</h2>
         </div>
-<<<<<<< HEAD
-        <div className='bothGreenContainer'>
-          <div className='leftGreenContainer'>
-            <p className='creamLeftAlign'>{pole_func}</p>
-          </div>
-          <div className='rightGreenContainer'>
-            <img
-              className='greenSection'
-              src={pole_func_img}
-              alt={pole_title}
-            />
-          </div>
-=======
       </div>
       <div className='bothGreenContainer'>
         <div className='leftGreenContainer'>
-          <p className='creamLeftAlign'>{pole_desc}</p>
->>>>>>> e7168e0c9a37a6a69ed0b5204a7e1a2c23afb8f8
+          <p className='creamLeftAlign'>{pole_func}</p>
         </div>
         <div className='rightGreenContainer'>
           <img className='greenSection' src={pole_func_img} alt={pole_title} />
