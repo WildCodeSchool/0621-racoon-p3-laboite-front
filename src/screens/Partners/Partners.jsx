2cb1e7a4150c5afbe668d5386da85666d7e929c0
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-09-29 16:47:18 +0200

    prettier 2

--- src/screens/Partners/Partners.jsx
+++ src/screens/Partners/Partners.jsx
@@ -16,11 +16,17 @@ import './Partners.css'
 const Partners = () => {
   const partnerTitle = "L'association et ses partenaires"
   const partnerPicto = 'hands-helping'
-  const creamText = ['Proposer aux personnes durablement privées d’emploi (PPDE) un emploi en contrat à durée indéterminée, à temps choisi, dans le cadre de l’expérimentation nationale « Territoire zéro chômeur de longue durée ».', 'Produire et vendre tous types de biens et/ou services auprès des particuliers, des professionnels, des associations et des collectivités territoriales.', 'Initier ou prendre part à toute initiative susceptible de favoriser le développement de l’emploi et corrélativement d’activités économiques sur le territoire.']
+  const creamText = [
+    'Proposer aux personnes durablement privées d’emploi (PPDE) un emploi en contrat à durée indéterminée, à temps choisi, dans le cadre de l’expérimentation nationale « Territoire zéro chômeur de longue durée ».',
+    'Produire et vendre tous types de biens et/ou services auprès des particuliers, des professionnels, des associations et des collectivités territoriales.',
+    'Initier ou prendre part à toute initiative susceptible de favoriser le développement de l’emploi et corrélativement d’activités économiques sur le territoire.'
+  ]
   const creamTitle = 'Sa mission'
   const creamPic = [fistbump]
-  const greenTitle = "Fonctionnement"
-  const greenText = ['Elle agit en collaboration avec un Comité Local pour l’Emploi dont les membres sont des élus et/ou des acteurs du territoire pour lesquels la lutte contre le chômage de longue durée est une priorité. Il est présidé par Fabien NEBEL, Maire de Bléré']
+  const greenTitle = 'Fonctionnement'
+  const greenText = [
+    'Elle agit en collaboration avec un Comité Local pour l’Emploi dont les membres sont des élus et/ou des acteurs du territoire pour lesquels la lutte contre le chômage de longue durée est une priorité. Il est présidé par Fabien NEBEL, Maire de Bléré'
+  ]
   const greenPic = [puzzle]
   const partnersList = ['pink', 'purple', 'orange', 'cyan', 'yellow']
 
@@ -41,12 +47,26 @@ const Partners = () => {
           <img src={homebanner} />
         </div> */}
         <RibbonTitle title={partnerTitle} picto={partnerPicto} />
-        <CreamBox title={creamTitle} text={creamText} pictures={creamPic}/>
-        <GreenBox title={greenTitle} text={greenText} pictures={greenPic}/>
-        <p className='cream paragraph'>LA BOÎTE D’À CÔTÉ a pu voir le jour grâce à l’engagement de nombreux bénévoles et de partenaires locaux avec lesquels elle garde des relations privilégiées :<br/>Commune de Bléré / Communauté de Communes Bléré-Val de Cher / Département d’Indre et Loire / Région Centre – Val de Loire / CRI (Château Renault Insertion) / Association LA MAIN TENDUE (Bléré) / Pôle Emploi / Chambres consulaires/ Rotary club d’Amboise (IL Y EN AURA D’AUTRES)</p>
+        <CreamBox title={creamTitle} text={creamText} pictures={creamPic} />
+        <GreenBox title={greenTitle} text={greenText} pictures={greenPic} />
+        <p className='cream paragraph'>
+          LA BOÎTE D’À CÔTÉ a pu voir le jour grâce à l’engagement de nombreux
+          bénévoles et de partenaires locaux avec lesquels elle garde des
+          relations privilégiées :
+          <br />
+          Commune de Bléré / Communauté de Communes Bléré-Val de Cher /
+          Département d’Indre et Loire / Région Centre – Val de Loire / CRI
+          (Château Renault Insertion) / Association LA MAIN TENDUE (Bléré) /
+          Pôle Emploi / Chambres consulaires/ Rotary club d’Amboise (IL Y EN
+          AURA D’AUTRES)
+        </p>
         <BlockPartners partners={partnersList} />
         <div className='green paragraph'>
-          <p>Si vous aussi, entreprise ou particulier, vous souhaitez apporter votre aide à LA BOÎTE D’À CÔTÉ et participer à la réussite de cette expérimentation, vous pouvez :</p>
+          <p>
+            Si vous aussi, entreprise ou particulier, vous souhaitez apporter
+            votre aide à LA BOÎTE D’À CÔTÉ et participer à la réussite de cette
+            expérimentation, vous pouvez :
+          </p>
           <ul>
             <li>Offrir votre temps en qualité de bénévole</li>
             <li>Faire une donation en numéraire ou en matériel</li>
