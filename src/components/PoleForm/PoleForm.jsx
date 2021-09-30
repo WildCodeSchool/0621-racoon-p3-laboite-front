db2277dcde256de7cc640f0ea621dfe5f6595628
fdmesquita <fannydemesquita@gmail.com>
2021-09-30 10:52:46 +0200

    start poleForm

---
+++ src/components/PoleForm/PoleForm.jsx
@@ -0,0 +1,15 @@
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
+import './PoleForm.css'
+
+const PoleForm = () => {
+  return (
+    <Form className=''>
+      <Form.Field>
+        <label>coucou</label>
+      </Form.Field>
+    </Form>
+  )
+}
+
+export default PoleForm
