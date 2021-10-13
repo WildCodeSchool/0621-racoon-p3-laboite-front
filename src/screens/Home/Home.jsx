8c0ebb86be40280fbf50ec7bd9a089aeaeebafeb
Benjamin1er <benjaminj.geron@gmail.com>
2021-10-13 11:55:24 +0200

    Voila

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -2,7 +2,6 @@ import axios from 'axios'
 import Concept from '../../components/Concept/Concept'
 import MemberCard from '../../components/Card/MemberCard'
 import PoleCard from '../../components/Card/PoleCard'
-import React from 'react'
 import RubanConcept from '../../components/Ruban/RubanConcept/RubanConcept'
 import { useState, useEffect } from 'react'
 
